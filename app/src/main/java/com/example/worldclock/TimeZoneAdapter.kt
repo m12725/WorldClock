73922474ff59300f25ed99f08fdbe331eef8e14f
Milly Kato <cmilly94@gmail.com>
2019-03-15 18:15:42 +0900

    added BaseAdapter

--- app/src/main/java/com/example/worldclock/TimeZoneAdapter.kt
+++ app/src/main/java/com/example/worldclock/TimeZoneAdapter.kt
@@ -1,4 +1,24 @@
 package com.example.worldclock
 
-class TimeZoneAdapter {
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class TimeZoneAdapter() : BaseAdapter() {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItem(position: Int): Any {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemId(position: Int): Long {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
